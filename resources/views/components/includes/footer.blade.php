67224702b91f91ebc44e9e09f043e1c75ce390a6
Saumy Anand <samcool3203@gmail.com>
2026-05-22 18:33:48 +0530

    fix: update contact email address in footer

--- resources/views/components/includes/footer.blade.php
+++ resources/views/components/includes/footer.blade.php
@@ -38,7 +38,7 @@
                     </li>
                     <li class="flex items-center gap-3 text-gray-400">
                         <svg class="w-5 h-5 text-orange-400 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/></svg>
-                        <a href="mailto:info@brahmanbhawan.com" class="hover:text-orange-400 transition">info@brahmanbhawan.com</a>
+                        <a href="mailto:brahminbhawandelhi@gmail.com" class="hover:text-orange-400 transition">brahminbhawandelhi@gmail.com</a>
                     </li>
                 </ul>
             </div>
